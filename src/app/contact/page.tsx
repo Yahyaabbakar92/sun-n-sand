e1ea8ce08dd49aa466a01fe4693d1583e2bccf08
Yahya <yahyaabbakar@gmail.com>
2024-02-06 16:18:41 +0200

    added contact page component

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,14 +1,20 @@
 import config from "@/config/config.json";
 import { getListPage } from "@/lib/contentParser";
-import PageHeader from "@/partials/PageHeader";
 import SeoMeta from "@/partials/SeoMeta";
 import { RegularPage } from "@/types";
+import { FaMapLocation } from "react-icons/fa6";
+import { FaPhone } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import { FaClock } from "react-icons/fa";
+import Image from "next/image";
+import image1 from "public/images/image-placeholder.png"
+import ScrollCardAnimation from "@/components/ScrollCardAnimation";
+import ContactForm from "@/layouts/ContactFormSection";
 
 const Contact = async () => {
   const data: RegularPage = getListPage("contact/_index.md");
   const { frontmatter } = data;
   const { title, description, meta_title, image } = frontmatter;
-  const { contact_form_action } = config.params;
 
   return (
     <>
@@ -18,54 +24,42 @@ const Contact = async () => {
         description={description}
         image={image}
       />
-      <PageHeader title={title} />
-      <section className="section-sm">
+      <div className="rounded-lg relative">
+        <Image
+          className="mx-auto mb-6"
+          src={image1}
+          width={1500}
+          height={1500}
+          alt="Hero image"
+        />
+        <h1
+          className="mb-6 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-fontColor"
+        >Contact & Reservations</h1>
+      </div>
+      <section className="mt-8 pb-8 bg-gradient-to-t from-sectionBackgroundColor to-white border-b border-border">
         <div className="container">
-          <div className="row">
-            <div className="mx-auto md:col-10 lg:col-6">
-              <form action={contact_form_action} method="POST">
-                <div className="mb-6">
-                  <label htmlFor="name" className="form-label">
-                    Full Name <span className="text-red-500">*</span>
-                  </label>
-                  <input
-                    id="name"
-                    name="name"
-                    className="form-input"
-                    placeholder="John Doe"
-                    type="text"
-                  />
-                </div>
-                <div className="mb-6">
-                  <label htmlFor="email" className="form-label">
-                    Working Mail <span className="text-red-500">*</span>
-                  </label>
-                  <input
-                    id="email"
-                    name="email"
-                    className="form-input"
-                    placeholder="john.doe@email.com"
-                    type="email"
-                  />
-                </div>
-                <div className="mb-6">
-                  <label htmlFor="message" className="form-label">
-                    Anything else? <span className="text-red-500">*</span>
-                  </label>
-                  <textarea
-                    id="message"
-                    name="message"
-                    className="form-input"
-                    placeholder="Message goes here..."
-                    rows={8}
-                  ></textarea>
-                </div>
-                <button type="submit" className="btn btn-primary">
-                  Submit
-                </button>
-              </form>
+          <ScrollCardAnimation>
+            <div className="lg:grid grid-cols-2 gap-8">
+              <div className="mx-auto mb-8 md:px-20 lg:px-0">
+                <h2 className="h2 text-fontColor">Contact Us</h2>
+                <ul className="mt-6 ">
+                  <li className="flex gap-2 items-center text-lg text-fontColor"><FaMapLocation className="text-accentColor" /> Address Along Lake Malawi Beach, Mangochi</li>
+                  <li className="flex gap-2 items-center text-lg text-fontColor mt-4"><FaPhone className="text-accentColor" /> Telephone: +265 996 492 399</li>
+                  <li className="flex gap-2 items-center text-lg text-fontColor mt-4"><MdEmail className="text-accentColor" /> E-mail: gm@sunnsandmw.com</li>
+                  <li className="flex gap-2 items-center text-lg text-fontColor mt-4"><FaClock className="text-accentColor" /> Office Hours: Monday – Sunday, 08:00 – 17:00</li>
+                </ul>
+                <Image
+                  className="mb-6 mt-8 rounded-lg"
+                  src={image1}
+                  width={600}
+                  height={300}
+                  alt="Hero image"
+                />
+                <p className="text-lg leading-8 mt-8 italic text-fontColor">We aim to respond to emails within 24 hours. Please check your spam folder if our response is not found in your inbox. For conference, group, or wedding bookings, email our reservations team directly using the listed contact details.</p>
+              </div>
+              <ContactForm />
             </div>
-          </div>
+          </ScrollCardAnimation>
         </div>
       </section>
     </>
